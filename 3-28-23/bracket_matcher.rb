eb4dfac52e8cc10cdbee14ed6ff5886e070a0055
Sid Mann <sjmann2@outlook.com>
2023-03-28 10:13:03 -0500

    Bracket matcher challenge

---
+++ 3-28-23/bracket_matcher.rb
@@ -0,0 +1,58 @@
+# Create a method/function that will intake a set of brackets [ { ( as a string 
+# and determine if the brackets are well-formed (match). Brackets can be nested.
+
+#   bracket('{}')
+#   // => true
+
+#   bracket('{()}')
+#   // => true
+
+#   bracket('({[]}{[]})') 
+#   // => true
+  
+#   bracket('{(')
+#   // => false
+  
+#   bracket('{[)][]}')
+#   // => false
+  
+#   bracket(']')
+#   // => false
+
+# set definition for each brackets 'pair' as a hash PAIRS = "{" => "}"
+# split the string in half, iterate through the first half of the string
+# For each bracket, grab its pair, store pairs in separate string
+# original halved string + separate string reversed should equal the original string
+# edge cases--
+  # open bracket must come first
+  # {(}) => false
+
+class BracketMatcher
+  PAIRS = {
+          '{' => '}',
+          '[' => ']',
+          '(' => ')',
+          ']' => '[',
+          '}' => '{',
+          ')' => '('
+        }
+          
+  def match?(brackets)
+    return false if brackets.length.odd?
+    first, second = brackets.each_char.each_slice(brackets.length / 2).map(&:join)
+    mirror = ''
+    first.each_char do |bracket|
+      mirror += PAIRS.fetch(bracket)
+    end
+    mirrored = first + mirror.reverse
+    brackets == mirrored
+  end
+end
+
+p BracketMatcher.new.match?('{}')
+p BracketMatcher.new.match?('{()}')
+p BracketMatcher.new.match?('({[]}{[]})')
+p BracketMatcher.new.match?('{[)][]}')
+p BracketMatcher.new.match?(']')
+p BracketMatcher.new.match?('{(})')
+p BracketMatcher.new.match?('{(')
